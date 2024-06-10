d354f86f7522939cd12582d809d353dea985b529
Rachael Babister <rachaelbabister@gmail.com>
2024-06-10 14:12:17 +0000

    Add function to Sidebar component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,10 @@
 import css from './App.module.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className={css.App}>
+      <Sidebar />
       {/* Add your components here */}
     </div>
   );
